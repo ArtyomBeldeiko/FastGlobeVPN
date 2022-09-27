033380e45eb7039dc41af4afcb7e08c378bcba44
ArtyomBeldeiko <artyom.beldeiko@gmail.com>
2022-09-27 23:53:39 +0300

    Initial Commit

---
+++ FastGlobeVPN/FastGlobeVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastGlobeVPNApp.swift
+//  FastGlobeVPN
+//
+//  Created by Artyom Beldeiko on 27.09.22.
+//
+
+import SwiftUI
+
+@main
+struct FastGlobeVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
